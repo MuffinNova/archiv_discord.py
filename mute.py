d7555bfb8187f04de81caff30ae008904dcd4bc8
MuffinNova <59145202+MuffinNova@users.noreply.github.com>
2020-02-18 18:57:40 +0100

    Update mute.py

--- mute.py
+++ mute.py
@@ -1,9 +1,8 @@
-@bot.command(pass_context = True)
-async def mute(ctx, user_id, userName: discord.User):
-    if ctx.message.author.server_permissions.administrator:
-        user = ctx.message.author
-        role = discord.utils.get(user.server.roles, name="Muted")
-        await bot.add_roles(user, role)
-     else:
-       embed=discord.Embed(title="Permission Denied.", description="You don't have permission to use this command.", color=0xff00f6)
-       await bot.say(embed=embed)
+@bot.command()
+async def mute(ctx, member: discord.Member=None):
+    role = discord.utils.get(ctx.guild.roles, names='Muted')
+    if not member:
+        await ctx.send('Please specify a member')
+        return
+    await member.add_roles(role)
+    await ctx.send('Added roles!')
