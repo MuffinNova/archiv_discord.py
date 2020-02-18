ff7523fd171eba5a0a954687f0f28bf35a4feff7
MuffinNova <59145202+MuffinNova@users.noreply.github.com>
2020-02-18 19:07:39 +0100

    Update mute.py

--- mute.py
+++ mute.py
@@ -1,8 +1,18 @@
 @bot.command()
-async def mute(ctx, member: discord.Member=None):
-    role = discord.utils.get(ctx.guild.roles, names='Muted')
-    if not member:
-        await ctx.send('Please specify a member')
-        return
-    await member.add_roles(role)
-    await ctx.send('Added roles!')
+async def mute(ctx, member : discord.Member):
+    guild = ctx.guild
+    
+    for role in guild.roles:
+        if role.name == 'Muted':
+            await member.add_roles(role)
+            await ctx.send('{} has {} has been muted'.format(member.mention, ctx.author.mention))
+            return
+            
+            overwrite = discord.PermissionsOverwrite(send_messages=False)
+            newRole = await guild.create_role(name='Muted')
+            
+            for channel in guild.text_channels:
+                await guild.set_permissions(newRole,overwrite=overwrite)
+                
+            await member.add_roles(newRole)
+            await ctx.send('{} has {} has been muted'.format(member.mention, ctx.author.mention))
