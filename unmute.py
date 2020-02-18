aa2e3029e41d1357f7d7089e98982aaa2d0f166b
MuffinNova <59145202+MuffinNova@users.noreply.github.com>
2020-02-18 19:20:11 +0100

    Add files via upload

---
+++ unmute.py
@@ -0,0 +1,8 @@
+@client.command()
+async def unmute(ctx, member : discord.Member)
+	guild = ctx.guild
+	
+	for role in guild.roles:
+		if role.name == 'Muted':
+			await member.remove_roles(role)
+			await ctx.send('{} has been unmuted'.format(member.mention))
